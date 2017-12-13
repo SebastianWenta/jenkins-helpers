66aded2728fa28cd4558a450a63fd2d62b09f4d9
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-13 08:59:20 +0100

    copyFile

--- vars/copyFile.groovy
+++ vars/copyFile.groovy
@@ -6,8 +6,8 @@ import java.nio.file.Paths
 def call (Object sourcePath, String targetPath){
 
   Path source = Paths.get(sourcePath.toString())
-  Path target = Paths.get(targetPath)
-  Files.copy(source, target + "\\target.txt", REPLACE_EXISTING)
+  Path target = Paths.get(targetPath + "\\target.txt")
+  Files.copy(source, target, REPLACE_EXISTING)
   return "asd"
   //return "RESULT FROM CALL: " +  "\n${sourcePath.toString()}" + "\n ${targetPath}"
 }
