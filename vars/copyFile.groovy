7ece8a343cfd7e9e88c3c65de165c260bbaed31b
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-13 08:58:19 +0100

    copyFile

--- vars/copyFile.groovy
+++ vars/copyFile.groovy
@@ -8,8 +8,8 @@ def call (Object sourcePath, String targetPath){
   Path source = Paths.get(sourcePath.toString())
   Path target = Paths.get(targetPath)
   Files.copy(source, target + "\\target.txt", REPLACE_EXISTING)
-
-  return "RESULT FROM CALL: " +  "\n${sourcePath.toString()}" + "\n ${targetPath}"
+  return "asd"
+  //return "RESULT FROM CALL: " +  "\n${sourcePath.toString()}" + "\n ${targetPath}"
 }
 
 
