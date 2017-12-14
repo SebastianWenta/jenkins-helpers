88b58821d51988b5c96d9ccd3c91e01f78be686a
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-14 13:06:57 +0100

    insertAttachment and db config

---
+++ vars/insertAttachment.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by sebwent on 14/12/2017.
+ */
+import pl.uniteam.rpa_dbconnect.*
+
+def call (Object sourcePath){
+        Attachments.insertAttachment(sourcePath.toString(), test, 15, asd, 1)
+}
