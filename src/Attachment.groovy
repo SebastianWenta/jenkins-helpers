39ee9f549c61170f1f5f8aadc74679b5fcdc0559
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-14 13:19:50 +0100

    Attachments class

---
+++ src/Attachment.groovy
@@ -0,0 +1,11 @@
+/**
+ * Created by sebwent on 14/12/2017.
+ */
+
+class Attachment {
+
+    public static def build() {
+        println "TEST XXX"
+    }
+
+}
--- vars/insertAttachment.groovy
+++ vars/insertAttachment.groovy
@@ -1,7 +1,7 @@
 /**
  * Created by sebwent on 14/12/2017.
  */
-//import pl.uniteam.rpa_dbconnect.*
+import pl.uniteam.rpa_dbconnect.*
 def call (Object sourcePath){
     //def Attachments att = new Attachments()
     //att.insertAttachment(sourcePath.toString(), test, 15, asd, 1)
