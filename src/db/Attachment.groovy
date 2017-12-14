11e6024a9baa67c2465290897cbde4fdccc658de
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-14 13:26:21 +0100

    Attachments class change

--- src/db/Attachment.groovy
+++ src/db/Attachment.groovy
@@ -2,9 +2,12 @@
  * Created by sebwent on 14/12/2017.
  */
 
-class Attachment {
+import pl.uniteam.rpa_dbconnect.*
 
+class Attachment {
     public static def build() {
+
+        Attachments att = new Attachments();
         return "TEST XXX"
     }
 
