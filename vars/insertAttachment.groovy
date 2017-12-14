2ad34dec77b1c0bb8e04c69e0384037f4f6d328f
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-14 13:13:53 +0100

    insertAttachment  change4

--- vars/insertAttachment.groovy
+++ vars/insertAttachment.groovy
@@ -1,8 +1,8 @@
 /**
  * Created by sebwent on 14/12/2017.
  */
-import pl.uniteam.rpa_dbconnect.*
+//import pl.uniteam.rpa_dbconnect.*
 def call (Object sourcePath){
-    def Attachments att = new Attachments()
+    //def Attachments att = new Attachments()
     //att.insertAttachment(sourcePath.toString(), test, 15, asd, 1)
 }
