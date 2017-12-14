6dfc00ae31cce889e7aaacbdf13ea4abb9cfb9bf
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-14 14:09:20 +0100

    Attachments class change3

--- src/db/Attachment.groovy
+++ src/db/Attachment.groovy
@@ -5,7 +5,7 @@ import pl.uniteam.rpa_dbconnect.*
 
 class Attachment {
     public static def build() {
-        this.getClass().classLoader.rootLoader.addURL(new File("rpa_dbconnect.jar").toURL())
+        //this.getClass().classLoader.rootLoader.addURL(new File("rpa_dbconnect.jar").toURL())
         Attachments att = new Attachments();
         return "TEST XXX"
     }
