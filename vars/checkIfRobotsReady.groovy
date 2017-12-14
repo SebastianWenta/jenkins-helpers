9bfaceedb4b2fb9884f8495fb4ab046a8cb467c0
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-14 11:26:02 +0100

    checkIfRobotsReady modification3

--- vars/checkIfRobotsReady.groovy
+++ vars/checkIfRobotsReady.groovy
@@ -4,20 +4,27 @@
 
 def call (){
 
+    String result = "INIT MESSAGE"
+
     def jobs = Jenkins.instance.getAllItems()
     jobs.findAll{j ->
-        j.fullName.contains("ROBOT")
+        j.fullName.contains("Child")
     }.each { j ->
         if (j instanceof com.cloudbees.hudson.plugins.folder.Folder) { return }
         println 'JOB: ' + j.fullName
+        result+='\nJOB: ' + j.fullName
         numbuilds = j.builds.size()
         if (numbuilds == 0) {
             println '  -> no build'
+            result+='\n  -> no build'
             return
         }
         lastbuild = j.builds[numbuilds - 1]
         buildNumber = numbuilds - 1
         println '  -> lastbuild: ' + buildNumber + " " + lastbuild.description + ' = ' + lastbuild.result + ', time: ' + lastbuild.timestampString2
+        result+='\n  -> lastbuild: ' + buildNumber + " " + lastbuild.description + ' = ' + lastbuild.result + ', time: ' + lastbuild.timestampString2
     }
+
+    return result
 
 }
