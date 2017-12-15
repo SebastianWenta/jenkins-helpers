cd9c1b3e523e24c1ee4596b8611a929ac908eb06
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-15 10:57:35 +0100

    readSimpleProperty

--- vars/createBuildInstance.groovy
+++ vars/createBuildInstance.groovy
@@ -5,10 +5,9 @@ import pl.uniteam.rpa_dbconnect.*
 
 def call (String process, String processDescription, int build){
     def processInstance = JenkinsProcess.findProcess(process, processDescription)
-    int buildId = Integer.parseInt(
-        JenkinsBuild.addNew(build,processInstance,"")
-                .readSimpleProperty(JenkinsBuild.PK_PK_COLUMN)
-                .toString()
-    )
+    def jenkinsBuild = JenkinsBuild.addNew(build,processInstance,"")
+
+    def pk = jenkinsBuild.getObjectId().getIdSnapshot().get(JenkinsBuild.PK_PK_COLUMN).toString()
+    int buildId = Integer.parseInt(pk)
     return buildId
 }
