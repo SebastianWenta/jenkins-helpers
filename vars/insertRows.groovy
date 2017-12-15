94cc12e0266c0677eb5f927440eeab2919f6ccfa
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-15 19:37:52 +0100

    1936

--- vars/insertRows.groovy
+++ vars/insertRows.groovy
@@ -16,23 +16,26 @@ import pl.uniteam.rpa_dbconnect.JenkinsBuild
 def call(String excel, int jenkinsBuildPK){
     Sheet sheet = getFirstSheetFromExcelWorkbook(new File (excel))
     ArrayList<ValueToInsert> fieldsValues = getAllFieldsValues(sheet)
-    Attachments att = JenkinsBuild.getJenkinsBuildByID(jenkinsBuildPK).getAttachmentIDRelation()
 
-    int maxRow = fieldsValues.collect {it.row}.max()
-    int minRow = fieldsValues.collect {it.row}.min()
+    return fieldsValues.toString()
 
-    minRow.upto(maxRow){ currentRow ->
-        Map<Fields, String> map = [:]
-
-        fieldsValues.findAll {it.row==currentRow}.each { currentField ->
-            println "CURRENT FIELD VALUES \n${currentField.column1}\n${currentField.column2}\n${currentField.column3}\n${currentField.value}"
-            Fields f = Fields.findField(currentField.column1, currentField.column2, currentField.column3)
-            println "CURRENT FIELD \n${f}"
-            map.put(f, currentField.value)
-        }
-
-        Data.insertValue(map, att, currentRow)
-    }
+//    Attachments att = JenkinsBuild.getJenkinsBuildByID(jenkinsBuildPK).getAttachmentIDRelation()
+//
+//    int maxRow = fieldsValues.collect {it.row}.max()
+//    int minRow = fieldsValues.collect {it.row}.min()
+//
+//    minRow.upto(maxRow){ currentRow ->
+//        Map<Fields, String> map = [:]
+//
+//        fieldsValues.findAll {it.row==currentRow}.each { currentField ->
+//            println "CURRENT FIELD VALUES \n${currentField.column1}\n${currentField.column2}\n${currentField.column3}\n${currentField.value}"
+//            Fields f = Fields.findField(currentField.column1, currentField.column2, currentField.column3)
+//            println "CURRENT FIELD \n${f}"
+//            map.put(f, currentField.value)
+//        }
+//
+//        Data.insertValue(map, att, currentRow)
+//    }
 
 }
 
@@ -120,81 +123,81 @@ static ArrayList<ValueToInsert> getAllFieldsValues(Sheet sheet){
             nearestFirstRow = excelItems.findAll {it.row==0 && it.column == availableColumns.min()}
         }
 
-//        if (logout){
-//            println "nearestSecondRow: " + (nearestSecondRow?.value?:"null") + " " + (nearestSecondRow?.column?:"0")
-//            println "nearestFirstRow: " + (nearestFirstRow?.value?:"null") + " " + (nearestFirstRow?.column?:"0")
-//        }
-//
-//        if ((nearestSecondRow?.column?:0) >= (nearestFirstRow?.column?:0)){
-//            thirdRowItem.parent = nearestSecondRow
-//        } else {
-//            thirdRowItem.parent = nearestFirstRow
-//        }
-//
-//        if (logout){
-//            println "thirdRowItem.row: " + thirdRowItem.row
-//            println "thirdRowItem.column: " + thirdRowItem.column
-//            println "thirdRowItem.value: " + thirdRowItem.value
-//            println "thirdRowItem.parent: " + thirdRowItem.parent.row + " : " + thirdRowItem.parent.column + " : " + thirdRowItem.parent.value
-//        }
+        if (logout){
+            println "nearestSecondRow: " + (nearestSecondRow?.value?:"null") + " " + (nearestSecondRow?.column?:"0")
+            println "nearestFirstRow: " + (nearestFirstRow?.value?:"null") + " " + (nearestFirstRow?.column?:"0")
+        }
+
+        if ((nearestSecondRow?.column?:0) >= (nearestFirstRow?.column?:0)){
+            thirdRowItem.parent = nearestSecondRow
+        } else {
+            thirdRowItem.parent = nearestFirstRow
+        }
+
+        if (logout){
+            println "thirdRowItem.row: " + thirdRowItem.row
+            println "thirdRowItem.column: " + thirdRowItem.column
+            println "thirdRowItem.value: " + thirdRowItem.value
+            println "thirdRowItem.parent: " + thirdRowItem.parent.row + " : " + thirdRowItem.parent.column + " : " + thirdRowItem.parent.value
+        }
     }
 
-//    rowIndex = 0;
-//    for (Iterator<Row> rowsIT = sheet.rowIterator(); rowsIT.hasNext();) {
-//        Row row = rowsIT.next();
-//        if (rowIndex>2){
-//            int cellIndex = 0;
-//            for (Iterator<Cell> cellsIT = row.cellIterator(); cellsIT.hasNext(); ) {
-//                Cell cell = cellsIT.next();
-//                if (logout){
-//                    println rowIndex + ":" + cellIndex + " - " + cell.toString()
-//                }
-//
-//                println "OPTIONS: "
-//                excelItems.findAll{it.column==cellIndex}.each { it ->
-//                    println "\n" + it.toString() + "\n"
-//                }
-//
-//                ExcelItem columnDescription
-//
-//                ExcelItem row2 = excelItems.findAll{it.column==cellIndex}.find{it.row==2}
-//                ExcelItem row1 = excelItems.findAll{it.column==cellIndex}.find{it.row==1}
-//                ExcelItem row0 = excelItems.findAll{it.column==cellIndex}.find{it.row==0}
-//
-//                if (row2!=null){
-//                    columnDescription = row2
-//                } else if (row1!=null){
-//                    columnDescription = row1
-//                } else if (row0!=null){
-//                    columnDescription = row0
-//                }
-//
-//                println "CHOSEN FOR COLUMN: \n" + columnDescription.toString() + "\n"
-//                println "CELL VALUE: " + cell.toString()
-//
-//                 if (columnDescription.parent != null){
-//                     if (columnDescription.parent.row==0){
-//                         if (columnDescription.row==2){
-//                             fields.add(new ValueToInsert(columnDescription.parent.value, "", columnDescription.value,cell.toString(), cellIndex,rowIndex))
-//                         } else {
-//                             fields.add(new ValueToInsert(columnDescription.parent.value, columnDescription.value, "",cell.toString(), cellIndex,rowIndex))
-//                         }
-//                     } else {
-//                         fields.add(new ValueToInsert(columnDescription.parent.parent.value, columnDescription.parent.value, columnDescription.value,cell.toString(), cellIndex,rowIndex))
-//                     }
-//                 } else {
-//                     fields.add(new ValueToInsert(columnDescription.value, "", "",cell.toString(), cellIndex,rowIndex))
-//                 }
-//
-//                cellIndex++;
-//            }
-//        }
-//        rowIndex++;
-//    }
-//
-//    fields.each{
-//        println it.toString()
-//    }
+    rowIndex = 0;
+    for (Iterator<Row> rowsIT = sheet.rowIterator(); rowsIT.hasNext();) {
+        Row row = rowsIT.next();
+        if (rowIndex>2){
+            int cellIndex = 0;
+            for (Iterator<Cell> cellsIT = row.cellIterator(); cellsIT.hasNext(); ) {
+                Cell cell = cellsIT.next();
+                if (logout){
+                    println rowIndex + ":" + cellIndex + " - " + cell.toString()
+                }
+
+                println "OPTIONS: "
+                excelItems.findAll{it.column==cellIndex}.each { it ->
+                    println "\n" + it.toString() + "\n"
+                }
+
+                ExcelItem columnDescription
+
+                ExcelItem row2 = excelItems.findAll{it.column==cellIndex}.find{it.row==2}
+                ExcelItem row1 = excelItems.findAll{it.column==cellIndex}.find{it.row==1}
+                ExcelItem row0 = excelItems.findAll{it.column==cellIndex}.find{it.row==0}
+
+                if (row2!=null){
+                    columnDescription = row2
+                } else if (row1!=null){
+                    columnDescription = row1
+                } else if (row0!=null){
+                    columnDescription = row0
+                }
+
+                println "CHOSEN FOR COLUMN: \n" + columnDescription.toString() + "\n"
+                println "CELL VALUE: " + cell.toString()
+
+                 if (columnDescription.parent != null){
+                     if (columnDescription.parent.row==0){
+                         if (columnDescription.row==2){
+                             fields.add(new ValueToInsert(columnDescription.parent.value, "", columnDescription.value,cell.toString(), cellIndex,rowIndex))
+                         } else {
+                             fields.add(new ValueToInsert(columnDescription.parent.value, columnDescription.value, "",cell.toString(), cellIndex,rowIndex))
+                         }
+                     } else {
+                         fields.add(new ValueToInsert(columnDescription.parent.parent.value, columnDescription.parent.value, columnDescription.value,cell.toString(), cellIndex,rowIndex))
+                     }
+                 } else {
+                     fields.add(new ValueToInsert(columnDescription.value, "", "",cell.toString(), cellIndex,rowIndex))
+                 }
+
+                cellIndex++;
+            }
+        }
+        rowIndex++;
+    }
+
+    fields.each{
+        println it.toString()
+    }
 
     return fields
 }
