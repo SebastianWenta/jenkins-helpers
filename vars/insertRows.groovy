8c8099bd72ca30f60c4f7bf85140a7481c7835a7
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-15 18:24:03 +0100

    1823

--- vars/insertRows.groovy
+++ vars/insertRows.groovy
@@ -2,18 +2,41 @@
  * Created by sebwent on 15/12/2017.
  */
 
-import groovy.json.JsonBuilder
-import groovy.json.JsonOutput
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.ss.usermodel.WorkbookFactory
+import pl.uniteam.rpa_dbconnect.Attachments
+import pl.uniteam.rpa_dbconnect.Data
+import pl.uniteam.rpa_dbconnect.Fields
+import pl.uniteam.rpa_dbconnect.JenkinsBuild
 
-def call(String excel){
+
+def call(String excel, int jenkinsBuildPK){
     Sheet sheet = getFirstSheetFromExcelWorkbook(new File (excel))
+    ArrayList<ValueToInsert> fieldsValues = getAllFieldsValues(sheet)
+    Attachments att = JenkinsBuild.getJenkinsBuildByID(jenkinsBuildPK).getAttachmentIDRelation()
+
+    int maxRow = fieldsValues.collect {it.row}.max()
+    int minRow = fieldsValues.collect {it.row}.min()
+
+    minRow.upto(maxRow){ currentRow ->
+        Map<Fields, String> map = [:]
+
+        fieldsValues.findAll {it.row==currentRow}.each { currentField ->
+            println "CURRENT FIELD VALUES \n${currentField.column1}\n${currentField.column2}\n${currentField.column3}\n${currentField.value}"
+            Fields f = Fields.findField(currentField.column1, currentField.column2, currentField.column3)
+            println "CURRENT FIELD \n${f}"
+            map.put(f, currentField.value)
+        }
+
+        Data.insertValue(map, att, currentRow)
+    }
+
 }
+
+//this.call("C:\\Users\\sebwent\\Desktop\\dane wejsciowe.xlsx", 200)
 
 
 static Sheet getFirstSheetFromExcelWorkbook(File excel){
@@ -22,18 +45,23 @@ static Sheet getFirstSheetFromExcelWorkbook(File excel){
     return workbook.getSheetAt(0)
 }
 
-static JsonBuilder getJsonRepresentation(Sheet sheet){
+static ArrayList<ValueToInsert> getAllFieldsValues(Sheet sheet){
+    boolean logout = false
+    ArrayList<ExcelItem> excelItems = new ArrayList<ExcelItem>()
+    ArrayList<ValueToInsert> fields = new ArrayList<>()
 
     if (logout){
         System.out.println("SHEET NAME: " + sheet);
     }
 
-
     int rowIndex = 0;
+    int columns = 0;
     for (Iterator<Row> rowsIT = sheet.rowIterator(); (rowsIT.hasNext() && (rowIndex<3));) {
         Row row = rowsIT.next();
+        columns = row.getPhysicalNumberOfCells()
         int cellIndex = 0;
         for (Iterator<Cell> cellsIT = row.cellIterator(); cellsIT.hasNext(); ) {
+
             Cell cell = cellsIT.next();
             if (logout){
                 println rowIndex + ":" + cellIndex + " - " + (String) cell.getStringCellValue()
@@ -92,38 +120,64 @@ static JsonBuilder getJsonRepresentation(Sheet sheet){
         }
     }
 
-    def json = JsonOutput.toJson(excelItems)
+    rowIndex = 0;
+    for (Iterator<Row> rowsIT = sheet.rowIterator(); rowsIT.hasNext();) {
+        Row row = rowsIT.next();
+        if (rowIndex>2){
+            int cellIndex = 0;
+            for (Iterator<Cell> cellsIT = row.cellIterator(); cellsIT.hasNext(); ) {
+                Cell cell = cellsIT.next();
+                if (logout){
+                    println rowIndex + ":" + cellIndex + " - " + cell.toString()
+                }
 
-    if (logout){
-        println json
-    }
+                println "OPTIONS: "
+                excelItems.findAll{it.column==cellIndex}.each { it ->
+                    println "\n" + it.toString() + "\n"
+                }
 
-    def jsonBuilder = new JsonBuilder()
+                ExcelItem columnDescription
 
-    jsonBuilder {
-        data excelItems.findAll{it.row==0}.collect {excelItem ->
-            [
-                    row : excelItem.row,
-                    column : excelItem.column,
-                    value : excelItem.value,
-                    children : excelItems.findAll{(it.row==1 || it.row==2)  && it.parent == excelItem}.collect { secondRow ->
-                        [
-                                row : secondRow.row,
-                                column : secondRow.column,
-                                value : secondRow.value,
-                                children : excelItems.findAll{it.row==2 && it.parent == secondRow}.collect { thirdRow ->
-                                    [
-                                            row : thirdRow.row,
-                                            column : thirdRow.column,
-                                            value : thirdRow.value
-                                    ]
-                                }
-                        ]
-                    }
+                ExcelItem row2 = excelItems.findAll{it.column==cellIndex}.find{it.row==2}
+                ExcelItem row1 = excelItems.findAll{it.column==cellIndex}.find{it.row==1}
+                ExcelItem row0 = excelItems.findAll{it.column==cellIndex}.find{it.row==0}
 
-            ]
+                if (row2!=null){
+                    columnDescription = row2
+                } else if (row1!=null){
+                    columnDescription = row1
+                } else if (row0!=null){
+                    columnDescription = row0
+                }
+
+                println "CHOSEN FOR COLUMN: \n" + columnDescription.toString() + "\n"
+                println "CELL VALUE: " + cell.toString()
+
+                 if (columnDescription.parent != null){
+                     if (columnDescription.parent.row==0){
+                         if (columnDescription.row==2){
+                             fields.add(new ValueToInsert(columnDescription.parent.value, "", columnDescription.value,cell.toString(), cellIndex,rowIndex))
+                         } else {
+                             fields.add(new ValueToInsert(columnDescription.parent.value, columnDescription.value, "",cell.toString(), cellIndex,rowIndex))
+                         }
+                     } else {
+                         fields.add(new ValueToInsert(columnDescription.parent.parent.value, columnDescription.parent.value, columnDescription.value,cell.toString(), cellIndex,rowIndex))
+                     }
+                 } else {
+                     fields.add(new ValueToInsert(columnDescription.value, "", "",cell.toString(), cellIndex,rowIndex))
+                 }
+
+                cellIndex++;
+            }
         }
+        rowIndex++;
     }
+
+    fields.each{
+        println it.toString()
+    }
+
+    return fields
 }
 
 
@@ -137,5 +191,42 @@ class ExcelItem{
         row = r
         column = c
         value = v
+    }
+
+    public String toString(){
+        "EXCELITEM \n" +
+                "row: ${this.row}\n" +
+                "column: ${this.column}\n" +
+                "value: ${this.value}\n" +
+                "parent: ${this.parent}"
+    }
+
+}
+
+class ValueToInsert {
+    String column1
+    String column2
+    String column3
+    String value
+    int column
+    int row
+
+    ValueToInsert(String c1, String c2, String c3, String v, int c, int r){
+        column1 = c1
+        column2 = c2
+        column3 = c3
+        value = v
+        column = c
+        row = r
+    }
+
+    public String toString(){
+        "VALUE TO INSERT \n" +
+                "column: ${this.column}\n" +
+                "row: ${this.row}\n" +
+                "column1: ${this.column1}\n" +
+                "column2: ${this.column2}\n" +
+                "column3: ${this.column3}\n" +
+                "value: ${this.value}\n"
     }
 }
