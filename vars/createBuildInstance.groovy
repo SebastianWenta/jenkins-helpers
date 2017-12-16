e9e1686c6a0224886259e09956c10843bc047d39
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-16 10:28:38 +0100

    1028

--- vars/createBuildInstance.groovy
+++ vars/createBuildInstance.groovy
@@ -5,8 +5,14 @@ import pl.uniteam.rpa_dbconnect.*
 import java.security.Timestamp
 
 def call (String process, String processDescription, int build){
-    def processInstance = JenkinsProcess.findProcess(process, processDescription)
-    def jenkinsBuild = JenkinsBuild.addNew(build,processInstance,Timestamp.toString())
-    def pk = jenkinsBuild.getObjectId().getIdSnapshot().get(JenkinsBuild.PK_PK_COLUMN).toString()
-    return Integer.parseInt(pk)
+    try {
+        def processInstance = JenkinsProcess.findProcess(process, processDescription)
+        def jenkinsBuild = JenkinsBuild.addNew(build,processInstance,Timestamp.toString())
+        def pk = jenkinsBuild.getObjectId().getIdSnapshot().get(JenkinsBuild.PK_PK_COLUMN).toString()
+        return Integer.parseInt(pk)
+    } catch (Exception e){
+        println e
+        return 0
+    }
+
 }
