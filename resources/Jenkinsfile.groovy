6d88b8aea16b0fc9e526da7eefc896d21bda4504
Sebastian Wenta <sebastian.wenta@gmail.com>
2017-12-19 12:51:56 +0100

    1251

--- resources/Jenkinsfile.groovy
+++ resources/Jenkinsfile.groovy
@@ -3,6 +3,7 @@
 int jenkinsBuildId
 String excelFile
 
+boolean isDBReady = false
 boolean isFileReady = false
 boolean isFileVerified = false
 boolean areRobotsReady = false
@@ -29,17 +30,44 @@ pipeline {
 
                     jenkinsBuildId = createBuildInstance(procesName, "description", buildNumber).toInteger()
                     echo "jenkinsBuildId: " + jenkinsBuildId
+
+                    if (jenkinsBuildId>0){
+                        isDBReady = true
+                    } else {
+                        currentBuild.result = 'FAILURE'
+                        currentBuild.setDescription("Problem with DB")
+                    }
                 }
             }
         }
         stage('Check if Robots are ready'){
+            when {
+                expression { isDBReady == true }
+            }
             steps{
                 script{
-                    echo checkIfRobotsReady()
+                    def isWindowsNodeReady = Jenkins.instance.getNode('Windows').toComputer().isOnline()
+                    echo "Is Windows node online: " + isWindowsNodeReady
+                    if (isWindowsNodeReady){
+                        def robotsResult = checkIfRobotsReady()
+                        if (robotsResult.contains("SUCCESS")){
+                            areRobotsReady = true
+                        } else {
+                            currentBuild.result = 'FAILURE'
+                            currentBuild.setDescription("Robots are not ready")
+                        }
+                    } else {
+                        currentBuild.result = 'FAILURE'
+                        currentBuild.setDescription("Robots are not online")
+                    }
+
                 }
             }
         }
         stage('Get Excel') {
+            when {
+                expression { areRobotsReady == true }
+            }
             steps {
                 script {
                     def inputFile = input message: 'Wybierz plik excel z danymi wejściowymi', parameters: [file (name:'data', description:'Plik powinien zawiera wpisy, które będa danymi wejściowymi do robotów.')]
@@ -51,8 +79,16 @@ pipeline {
                 success {
                     echo "Uploading to DB"
                     script{
-                        insertAttachment(excelFile, jenkinsBuildId)
-                        isFileReady = true
+                        echo "excelFile: " + excelFile
+                        echo "jenkinsBuildId: " + jenkinsBuildId
+                        def att = insertAttachment(excelFile, jenkinsBuildId)
+                        if (att.contains("SUCCESS")){
+                            isFileReady = true
+                        } else {
+                            currentBuild.result = 'FAILURE'
+                            currentBuild.setDescription("Attachment not inserted to DB")
+                        }
+
                     }
                 }
                 failure {
@@ -66,16 +102,19 @@ pipeline {
             }
             steps{
                 script{
-                    String excelHandlerResult = bat(script: 'java -jar excelHandler-all.JAR target.xlsx Umowy.config', returnStdout: true)
+                    String excelHandlerResult = bat(script: 'java -jar excelHandler-all.JAR verify target.xlsx Umowy.config', returnStdout: true)
                     echo "RESULT: " + excelHandlerResult
                     if (excelHandlerResult.contains("SUCCESS")){
                         isFileVerified = true
+                    } else {
+                        currentBuild.result = 'FAILURE'
+                        currentBuild.setDescription("Excel verification failed")
                     }
                 }
             }
             post {
                 always{
-                    echo "Verification update to DB"
+                    echo "POST Function ater excel Verification"
                 }
             }
         }
@@ -84,11 +123,41 @@ pipeline {
                 expression { isFileVerified == true }
             }
             steps {
-                echo "Doing Some Job"
+                script {
+                    String excelHandlerResult = bat(script: "java -jar excelHandler-all.JAR insert \"${excelFile}\" ${jenkinsBuildId}", returnStdout: true)
+                    echo "RESULT: " + excelHandlerResult
+                    if (excelHandlerResult.contains("SUCCESS")){
+                        areRecordsImportedToDB = true
+                    } else {
+                        currentBuild.result = 'FAILURE'
+                        currentBuild.setDescription("Records not imported to DB")
+                    }
+                }
             }
             post {
                 always {
                     echo "POST Function ater Importing"
+                }
+            }
+        }
+        stage('Robot'){
+            when {
+                expression { areRecordsImportedToDB == true }
+            }
+            steps {
+                node('Windows') {
+                    echo "Get Robot source code"
+                    git credentialsId: '3e55bd01-346f-40c4-8c41-095e438689c2', url: 'https://github.com/SebastianWenta/jenkins-helpers'
+                    script{
+                        for (int i = 0; i <10; i++){
+                            echo "current trial: ${i}"
+                        }
+                    }
+                }
+            }
+            post {
+                always {
+                    echo "POST Function ater robot"
                 }
             }
         }
